a4216a37df391343f2186ea493253d72b67b033f
Tomas Valdes <tvaldes60@gmail.com>
2020-12-16 18:16:41 -0600

    Template for Ceasershift
    It's not necessary to follow this template, it's just there so groups can have an easier time collaberating.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Enter in the message you want to encrypt");
+        String /*Your variable name here*/= input.nextLine();
+        System.out.println("Enter in the amount you want to shift the message");
+        int /*Your variable name here*/= input.nextInt();
+        System.out.println("Enter in the amount of letters per group");
+        int /*Your variable name here*/=input.nextInt();
+
+        System.out.println("Your encrypted message is: " /*Your message here*/);
+    }
+
+
+    public static /*Your data type here*/ normalizeText(/*Your parameters here*/){
+        return ; //Return your specified data type here
+    }
+
+    public static /*Your data type here*/ ceaserify(/*Your parameters here*/){
+        return; //Return your specified data type here
+    }
+
+    public static /*Your data type here*/ codeGrouper(/*Your parameters here*/){
+        return; //Return your specified data type here
+    }
+
+    public static /*Your data type here*/ finalizeMessage(/*Your parameters here*/){
+        return ; //Return your specified data type here
     }
 }
